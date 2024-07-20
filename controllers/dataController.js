5b69b185897768f007df95be75a9e2edb6bfb1b7
MasterHiggins <151066454+MasterHiggins@users.noreply.github.com>
2024-07-20 21:04:03 -0230

    Minor fixes

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -3,7 +3,7 @@ const pool = require('../models/data');
 //This entire file mostly just uses SQL create statements to fulfill each request (Defined in const's)
 const getAllData = async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM your_table_name');
+        const result = await pool.query('SELECT * FROM user_emails');
         res.json(result.rows);
     } catch (err) {
         res.status(500).send(err);
@@ -14,7 +14,7 @@ const createData = async (req, res) => {
     const { name, email } = req.body;
     try {
         const result = await pool.query(
-            'INSERT INTO your_table_name (name, email) VALUES ($1, $2) RETURNING *',
+            'INSERT INTO user_emails (name, email) VALUES ($1, $2) RETURNING *',
             [name, email]
         );
         res.status(201).json(result.rows[0]);
@@ -28,7 +28,7 @@ const updateData = async (req, res) => {
     const { name, email } = req.body;
     try {
         const result = await pool.query(
-            'UPDATE your_table_name SET name = $1, email = $2 WHERE id = $3 RETURNING *',
+            'UPDATE user_emails SET name = $1, email = $2 WHERE id = $3 RETURNING *',
             [name, email, id]
         );
         res.json(result.rows[0]);
@@ -40,7 +40,7 @@ const updateData = async (req, res) => {
 const deleteData = async (req, res) => {
     const { id } = req.params;
     try {
-        await pool.query('DELETE FROM your_table_name WHERE id = $1', [id]);
+        await pool.query('DELETE FROM user_emails WHERE id = $1', [id]);
         res.status(204).send();
     } catch (err) {
         res.status(500).send(err);
